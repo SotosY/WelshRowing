74214d14bab152af0220114e7f6cbbc843f06601
Sotiris Yiallourides <yiallouridess@cardiff.ac.uk>
2021-03-13 15:26:15 +0000

    Update WelshRowingApplicationTests.java

--- src/test/java/com/team1/welshrowing/WelshRowingApplicationTests.java
+++ src/test/java/com/team1/welshrowing/WelshRowingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WelshRowingApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    // @Test
+    // void contextLoads() {
+    // }
 
 }
